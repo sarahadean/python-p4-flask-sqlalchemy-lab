a04e69a4f75c1fd044afae577d911535bd668e0e
Sarah Dean <128323898+sarahadean@users.noreply.github.com>
2023-06-13 13:05:26 -0500

    passed tests

--- server/app.py
+++ server/app.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+#flask db init has already been run. 
+#You will need to direct your Flask app to a database at app.db, create models, 
+#run a migration with flask db revision --autogenerate -m'<your message>' and create the database file with flask db upgrade.
+
 from flask import Flask, make_response
 from flask_migrate import Migrate
 
@@ -19,15 +23,46 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.filter(Animal.id == id).first()
+    display = f''
+    display += f'<ul>ID: {animal.id}</ul>'
+    display += f'<ul>Name: {animal.name}</ul>'
+    display += f'<ul>Species: {animal.species}</ul>'
+    display += f'<ul>Zookeeper: {animal.zookeeper.name}</ul>'
+    display += f'<ul>Enclosure: {animal.enclosure.environment}</ul>'
+    return make_response(display)
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.filter(Zookeeper.id == id).first()
+    #zookeeper contains:
+    # - list of animals
+    display = f''
+    display += f'<ul>ID: {zookeeper.id}</ul>'
+    display += f'<ul>Name: {zookeeper.name}</ul>'
+    display += f'<ul>Birthday: {zookeeper.birthday}</ul>'
 
+    for animal in zookeeper.animals:
+        display += f'<ul>Animal:{animal.name}</ul>'
+
+    return make_response(display)
+
+# id = db.Column(db.Integer, primary_key=True)
+#     environment = db.Column(db.String)
+#     open_to_visitors = db.Column(db.Boolean)
+
+#     animals = db.relationship('Animal', back_populates='enclosure')
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    enclosure = Enclosure.query.filter(Enclosure.id == id).first()
+    display = f''
+    display += f'<ul>Id: {enclosure.id}</ul>'
+    display += f'<ul>Environment: {enclosure.environment}</ul>'
+    display += f'<ul>Open to Visitors: {enclosure.open_to_visitors}</ul>'
+    for animal in enclosure.animals:
+        display += f'<ul>Animal: {animal.name}</ul>'
+
+    return make_response(display)
 
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -1,3 +1,23 @@
+# '''
+# Your database should represent a zoo. There should be three tables: animals, zookeepers, and enclosures.
+# The Animal model should contain: 
+# a name, 
+# a species, 
+# a zookeeper, and 
+# an enclosure.
+
+# The Zookeeper model should contain 
+# a name, 
+# a birthday, and 
+# a list of animals that they take care of.
+
+# The Enclosure model should contain 
+# an environment (grass, sand, or water), 
+# an open_to_visitors boolean, 
+# and a list of animals.
+# '''
+
+
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 
@@ -11,13 +31,30 @@ class Zookeeper(db.Model):
     __tablename__ = 'zookeepers'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    birthday = db.Column(db.String)
+
+    animals = db.relationship('Animal', back_populates='zookeeper')
 
 class Enclosure(db.Model):
     __tablename__ = 'enclosures'
 
     id = db.Column(db.Integer, primary_key=True)
+    environment = db.Column(db.String)
+    open_to_visitors = db.Column(db.Boolean)
 
+    animals = db.relationship('Animal', back_populates='enclosure')
+    
 class Animal(db.Model):
     __tablename__ = 'animals'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    species = db.Column(db.String)
+
+    zookeeper_id = db.Column(db.Integer, db.ForeignKey('zookeepers.id'))
+    enclosure_id = db.Column(db.Integer, db.ForeignKey('enclosures.id'))
+
+    # roles = db.relationship('Role', back_populates='production')
+    zookeeper = db.relationship('Zookeeper', back_populates='animals')
+    enclosure = db.relationship('Enclosure', back_populates='animals')
